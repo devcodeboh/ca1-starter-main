a56ed9fb3f14735e2333bbfc68e458f7a8787e56
Bohdan Husiev <bohdan.husiev+stoys@storytoys.com>
2026-03-07 18:30:07 +0000

    feat(data): add app metadata, contact and about stats store

--- models/app-store.js
+++ models/app-store.js
@@ -1,18 +1,30 @@
 'use strict';
 
-import logger from '../utils/logger.js';
 import JsonStore from './json-store.js';
 
 const appStore = {
-
   store: new JsonStore('./models/app-store.json', { info: {} }),
   collection: 'info',
-  array: 'creators',
 
   getAppInfo() {
     return this.store.findAll(this.collection);
   },
 
+  getAuthorInfo() {
+    return this.getAppInfo().author;
+  },
+
+  getMapInfo() {
+    const info = this.getAppInfo();
+    return {
+      image: info.mapImage,
+      alt: info.mapImageAlt,
+    };
+  },
+
+  getStaticStats() {
+    return this.getAppInfo().statistics;
+  },
 };
 
 export default appStore;
