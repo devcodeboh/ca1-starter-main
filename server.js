5351e0be20c6ded9370ab6d928567412c65f635d
Bohdan Husiev <bohdan.husiev+stoys@storytoys.com>
2026-05-03 11:03:14 +0100

    Add form session and upload middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 import routes from "./routes.js";
 import logger from "./utils/logger.js";
 import { create } from 'express-handlebars';
@@ -9,6 +11,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'volcano-watch-dev-secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 3,
+  },
+}));
 
 const handlebars = create({extname: '.hbs'});
 app.engine(".hbs", handlebars.engine);
